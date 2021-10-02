9847445e126a9159b2c0fe36b9c7c3e060b7afec
Stonek79 <amit-yurist@mail.ru>
2021-10-02 18:33:48 +0300

    ObjectValidator and test refactoring

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -5,43 +5,43 @@ describe('Validators:', () => {
     const v = new Validator();
     const schema = v.string();
 
-    expect(schema.isValid('test data')).toBeTruthy();
-    expect(schema.isValid(null)).toBeTruthy();
-    expect(schema.isValid('')).toBeTruthy();
+    expect(schema.isValid('test data')).toBe(true);
+    expect(schema.isValid(null)).toBe(true);
+    expect(schema.isValid('')).toBe(true);
 
     schema.required();
-    expect(schema.isValid('data')).toBeTruthy();
-    expect(schema.isValid('')).toBeFalsy();
-    expect(schema.isValid(null)).toBeFalsy();
+    expect(schema.isValid('data')).toBe(true);
+    expect(schema.isValid('')).toBe(false);
+    expect(schema.isValid(null)).toBe(false);
+
+    schema.minLength(11);
+    expect(schema.isValid('normal length')).toBe(true);
+    expect(schema.isValid('min length')).toBe(false);
 
     schema.contains('data');
-    expect(schema.isValid('this is data')).toBeTruthy();
-    expect(schema.isValid('this is false')).toBeFalsy();
-
-    schema.minLength(10);
-    expect(schema.isValid('this is data')).toBeTruthy();
-    expect(schema.isValid('the data')).toBeFalsy();
+    expect(schema.isValid('this is data')).toBe(true);
+    expect(schema.isValid('this is false')).toBe(false);
   });
 
   test('number', () => {
     const v = new Validator();
     const schema = v.number();
 
-    expect(schema.isValid(100)).toBeTruthy();
-    expect(schema.isValid(null)).toBeTruthy();
-    expect(schema.isValid(0)).toBeTruthy();
+    expect(schema.isValid(100)).toBe(true);
+    expect(schema.isValid(null)).toBe(true);
+    expect(schema.isValid(0)).toBe(true);
 
     schema.required();
-    expect(schema.isValid(99)).toBeTruthy();
-    expect(schema.isValid(-99)).toBeTruthy();
-    expect(schema.isValid()).toBeFalsy();
-    expect(schema.isValid(null)).toBeFalsy();
+    expect(schema.isValid(99)).toBe(true);
+    expect(schema.isValid(-99)).toBe(true);
+    expect(schema.isValid()).toBe(false);
+    expect(schema.isValid(null)).toBe(false);
 
     schema.range(-10, 42);
-    expect(schema.isValid(7)).toBeTruthy();
-    expect(schema.isValid(-10)).toBeTruthy();
-    expect(schema.isValid(-11)).toBeFalsy();
-    expect(schema.isValid(66)).toBeFalsy();
+    expect(schema.isValid(7)).toBe(true);
+    expect(schema.isValid(-10)).toBe(true);
+    expect(schema.isValid(-11)).toBe(false);
+    expect(schema.isValid(66)).toBe(false);
   });
 
   test('number positive', () => {
@@ -49,37 +49,38 @@ describe('Validators:', () => {
     const schema = v.number();
 
     schema.positive();
-    expect(schema.isValid(0)).toBeFalsy();
-    expect(schema.isValid(10)).toBeTruthy();
-    expect(schema.isValid(-2)).toBeFalsy();
-    expect(schema.isValid(null)).toBeTruthy();
+    expect(schema.isValid(0)).toBe(false);
+    expect(schema.isValid(10)).toBe(true);
+    expect(schema.isValid(-2)).toBe(false);
+    expect(schema.isValid(null)).toBe(true);
   });
 
   test('array', () => {
     const v = new Validator();
     const schema = v.array();
 
-    expect(schema.isValid([])).toBeTruthy();
+    expect(schema.isValid([])).toBe(true);
+    expect(schema.isValid(null)).toBe(true);
 
     schema.required();
-    expect(schema.isValid([])).toBeTruthy();
-    expect(schema.isValid([99])).toBeTruthy();
-    expect(schema.isValid()).toBeFalsy();
-    expect(schema.isValid(null)).toBeFalsy();
+    expect(schema.isValid([])).toBe(true);
+    expect(schema.isValid([99])).toBe(true);
+    expect(schema.isValid()).toBe(false);
+    expect(schema.isValid(null)).toBe(false);
 
     schema.sizeof(3);
-    expect(schema.isValid([1, 2, 3, 4, 5])).toBeTruthy();
-    expect(schema.isValid(['one', 'two', 3])).toBeTruthy();
-    expect(schema.isValid(['one'])).toBeFalsy();
-    expect(schema.isValid([])).toBeFalsy();
+    expect(schema.isValid([1, 2, 3, 4, 5])).toBe(true);
+    expect(schema.isValid(['one', 'two', 3])).toBe(true);
+    expect(schema.isValid(['one'])).toBe(false);
+    expect(schema.isValid([])).toBe(false);
   });
 
   test('shape (object)', () => {
     const v = new Validator();
     const schema = v.object();
 
-    expect(schema.isValid()).toBeTruthy();
-    expect(schema.isValid(null)).toBeTruthy();
+    expect(schema.isValid()).toBe(true);
+    expect(schema.isValid(null)).toBe(true);
 
     schema.shape({
       car: v.string().required(),
@@ -87,10 +88,10 @@ describe('Validators:', () => {
       models: v.array().required().sizeof(1),
     });
 
-    expect(schema.isValid({ car: 'dodge', year: 1969, models: ['charger'] })).toBeTruthy();
-    expect(schema.isValid({ car: 'mersedes', year: null, models: ['benz'] })).toBeFalsy();
-    expect(schema.isValid({ car: 'lada', year: 1999, models: 'granta' })).toBeFalsy();
-    expect(schema.isValid({ car: 'telega', year: 1488, models: ['horse', 'donkey'] })).toBeFalsy();
+    expect(schema.isValid({ car: 'dodge', year: 1969, models: ['charger'] })).toBe(true);
+    expect(schema.isValid({ car: 'mersedes', year: '1909', models: ['benz'] })).toBe(false);
+    expect(schema.isValid({ car: 'lada', year: 1999, models: 'granta' })).toBe(false);
+    expect(schema.isValid({ car: 'telega', year: 1488, models: ['horse', 'donkey'] })).toBe(false);
   });
 
   test('custom validator', () => {
@@ -106,19 +107,19 @@ describe('Validators:', () => {
     v.addValidator('string', 'endsWith', fn3);
 
     const schema1 = v.string().test('notEndsSpace');
-    expect(schema1.isValid('This is true')).toBeTruthy();
-    expect(schema1.isValid('This is false ')).toBeFalsy();
-    expect(schema1.isValid('This is false too   ')).toBeFalsy();
+    expect(schema1.isValid('This is true')).toBe(true);
+    expect(schema1.isValid('This is false ')).toBe(false);
+    expect(schema1.isValid('This is false too   ')).toBe(false);
 
     const schema2 = v.number().test('isInteger');
-    expect(schema2.isValid(5)).toBeTruthy();
-    expect(schema2.isValid(-5)).toBeTruthy();
-    expect(schema2.isValid(2.5)).toBeFalsy();
-    expect(schema2.isValid(-2.5)).toBeFalsy();
+    expect(schema2.isValid(5)).toBe(true);
+    expect(schema2.isValid(-5)).toBe(true);
+    expect(schema2.isValid(2.5)).toBe(false);
+    expect(schema2.isValid(-2.5)).toBe(false);
 
     const schema3 = v.string().test('endsWith', '!');
-    expect(schema3.isValid('This is the end!')).toBeTruthy();
-    expect(schema3.isValid('This is not the end! ')).toBeFalsy();
-    expect(schema3.isValid('This is not the end too')).toBeFalsy();
+    expect(schema3.isValid('This is the end!')).toBe(true);
+    expect(schema3.isValid('This is not the end! ')).toBe(false);
+    expect(schema3.isValid('This is not the end too')).toBe(false);
   });
 });
--- src/ObjectValidator.js
+++ src/ObjectValidator.js
@@ -1,14 +1,27 @@
-export default class ObjectValidator {
-  shape(obj) {
-    this.schema = obj;
+import _ from 'lodash';
+
+import BaseValidationSchema from "./BaseValidationSchema";
+
+export default class ObjectValidator extends BaseValidationSchema {
+  constructor(customValidators) {
+    super(Object.assign(ObjectValidator.validators, customValidators));
+    this.checks = [];
+  }
+
+  shape(schemas) {
+    const args = [];
+    const validate = (data) => {
+      args.push(data);
+
+      return Object.keys(data).every((key) => schemas[key].isValid(data[key]));
+    };
+
+    this.checks.push({ validate, args });
 
     return this;
   }
+}
 
-  isValid(data = []) {
-    if (!data) return true;
-    const keys = Object.keys(data);
-
-    return keys.every((key) => this.schema[key].isValid(data[key]));
-  }
+ObjectValidator.validators = {
+  validate: (arg, fn) => fn(arg),
 }
